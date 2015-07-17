66c082d68d3bbdf3ccaf88a3242aa8705c8f03bb
steamprodz <andreydmg@gmail.com>
2015-07-17 15:08:45 +0300

    Ver.2. Added User Control and some interface

---
+++ DantistApp/GroupedImage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DantistApp
+{
+    /// <summary>
+    /// Interaction logic for GroupedImage.xaml
+    /// </summary>
+    public partial class GroupedImage : UserControl
+    {
+        //Currently moving object
+        private Image MovingImage;
+
+        // Mouse tools
+        private Point MousePosition;
+
+        public GroupedImage()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public ImageSource SourceTop
+        {
+            get { return base.GetValue(SourceTopProperty) as ImageSource; }
+            set { base.SetValue(SourceTopProperty, value); }
+        }
+
+        public ImageSource SourceBot
+        {
+            get { return base.GetValue(SourceBotProperty) as ImageSource; }
+            set { base.SetValue(SourceBotProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceTopProperty =
+            DependencyProperty.Register("SourceTop", typeof(ImageSource), typeof(GroupedImage));
+
+        public static readonly DependencyProperty SourceBotProperty =
+            DependencyProperty.Register("SourceBot", typeof(ImageSource), typeof(GroupedImage));
+
+        private void CanvasMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var image = e.Source as Image;
+
+            if (image != null && controlCanvas.CaptureMouse())
+            {
+                MousePosition = e.GetPosition(controlCanvas);
+                MovingImage = image;
+                //Panel.SetZIndex(MovingImage, 1); // in case of multiple images
+            }
+        }
+
+        private void CanvasMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (MovingImage != null)
+            {
+                controlCanvas.ReleaseMouseCapture();
+                //Panel.SetZIndex(MovingImage, 0);
+                MovingImage = null;
+            }
+        }
+
+        private void CanvasMouseMove(object sender, MouseEventArgs e)
+        {
+            if (MovingImage != null)
+            {
+                var position = e.GetPosition(controlCanvas);
+                var offset = position - MousePosition;
+                MousePosition = position;
+                Canvas.SetLeft(MovingImage, Canvas.GetLeft(MovingImage) + offset.X);
+                Canvas.SetTop(MovingImage, Canvas.GetTop(MovingImage) + offset.Y);
+            }
+        }
+
+    }
+}
