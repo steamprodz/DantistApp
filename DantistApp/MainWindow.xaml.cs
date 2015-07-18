10d82d196d94aad5ab572607e4ef3f996bbed7c8
MrMixxxeR <rudyakov.yuriy@gmail.com>
2015-07-18 16:37:35 +0300

    MainWindow tabcontrol switching

--- DantistApp/MainWindow.xaml.cs
+++ DantistApp/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using DantistApp.Elements;
+using System.Text.RegularExpressions;
 
 namespace DantistApp
 {
@@ -28,22 +29,31 @@ namespace DantistApp
         {
             InitializeComponent();
 
+            foreach (var item in panel_btns.Children)
+            {
+                if (item is Button)
+                    (item as Button).Click += PanelBtn_Click;
+            }
+
             foreach (var item in grid_nij_chel.Children)
             {
                 if (item is UnlimitedElement)
-                {
-                    UnlimitedElement element = item as UnlimitedElement;
-                    element.MouseLeftButtonDown += UnlimitedElement_Adding;
-                }
+                    (item as UnlimitedElement).MouseLeftButtonDown += UnlimitedElement_Adding;
             }
             foreach (var item in grid_ver_chel.Children)
             {
                 if (item is GroupElement)
-                {
-                    GroupElement element = item as GroupElement;
-                    element.MouseLeftButtonDown += GroupElement_Adding;
-                }
+                    (item as GroupElement).MouseLeftButtonDown += GroupElement_Adding;
             }
+        }
+
+        private void PanelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            String subStr = btn.Content.ToString().Substring(0, 3);
+            int number = Convert.ToInt32(Regex.Match(subStr, @"\d+").Value);
+
+            tabControl_elements.SelectedIndex = number - 1;
         }
 
 
