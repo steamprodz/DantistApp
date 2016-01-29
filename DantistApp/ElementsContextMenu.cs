a2b347ec86c6bd83741ef845d3c4b7e0787d2218
Andrii Diachenko <andreydmg@gmail.com>
2016-01-29 17:27:51 +0200

    Root seal fixes (see win7 branch)

--- DantistApp/ElementsContextMenu.cs
+++ DantistApp/ElementsContextMenu.cs
@@ -555,6 +555,7 @@ namespace DantistApp
                         canvas.Children.Remove(composite.RootSeal);
 
                     composite.RootSeal = new Element();
+                    composite.RootSealMoving = true;
                     if (composite.RelativeElement != null)
                         composite.RelativeElement.RootSeal = composite.RootSeal;
 
@@ -606,6 +607,7 @@ namespace DantistApp
                         canvas.Children.Remove(composite.RootSeal);
 
                     composite.RootSeal = new Element();
+                    composite.RootSealMoving = true;
                     if (composite.RelativeElement != null)
                         composite.RelativeElement.RootSeal = composite.RootSeal;
 
